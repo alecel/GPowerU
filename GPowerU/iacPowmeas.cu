d1688bb8a62fab4ac15d220901cf894a134af998
Alessandro Celestini <a.celestini@gmail.com>
2024-12-13 16:01:37 +0100

    update

--- GPowerU/iacPowmeas.cu
+++ GPowerU/iacPowmeas.cu
@@ -6,12 +6,18 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <signal.h>     
+#include <getopt.h>
+
+#define USAGE                                                                                          \
+    "Usage: %s --output-dir <DIR_NAME> \n\n"                                                           \
+    "\t-o, --output-dir <DIR>              Write power profile data files to <DIR_NAME>.\n\n"
+
 
 
 static void sigHandler(int signum){
     if (signum == SIGINT){
-        fprintf ( stderr, "SIGINT catched!\n" );
-        printf("Ready to exit!\n");
+        fprintf( stderr, "SIGINT catched!\n" );
+        fprintf( stderr, "Ready to exit!\n");
         if ( GPowerU_end(5) != 0 ){
             fprintf ( stderr, " error: terminating...\n" );
             _exit (1);
@@ -23,23 +29,50 @@ static void sigHandler(int signum){
  
 
 int main( int argc, char** argv){
+   
+  char node_name[256];	
+  char *out_dir=NULL;
+  int opt;	
+  static struct option long_options[] = {
+        { "output-dir", required_argument, NULL, 'o' }
+  };
 
-    //Initializations ==> enable the NVML library, starts CPU thread for the power monitoring,  
-    if ( GPowerU_init() != 0 ) {
-        fprintf ( stderr, "%s: error: initializing...\n", argv[0] );
-        _exit (1);
-     }
-
-    if (signal(SIGINT, sigHandler) == SIG_ERR){
-        printf("Error, cannot handle SIGINT\n");
+  while ((opt = getopt_long(argc, argv, "o:h", long_options, NULL)) != -1) {
+    switch (opt) {
+     case 'o':
+       out_dir = strdup(optarg);
+       break;
+     case 'h':
+     default:
+       printf(USAGE, argv[0]);
+       exit(EXIT_FAILURE);
     }
+  }
+  
+  if ( out_dir == NULL){
+    printf(USAGE, argv[0]);
+    exit(EXIT_FAILURE);
+  }
 
-    printf("Start FOR\n");
-    for(;;){
-       sleep(30);
-    }
-    fprintf( stderr, "ERROR: End FOR!!!\n");
+  memset(node_name, 0, 256);
+  gethostname(node_name, 256);
+  
+  printf("out_dir: %s --- node_name: %s \n", out_dir, node_name);
 
-    return 0;
+  //Initializations ==> enable the NVML library, starts CPU thread for the power monitoring,  
+  if ( GPowerU_init(out_dir, node_name) != 0 ) {
+    fprintf ( stderr, "%s: error: initializing...\n", argv[0] );
+    _exit (1);
+  }
+
+  if (signal(SIGINT, sigHandler) == SIG_ERR){
+    printf("Error, cannot handle SIGINT\n");
+  }
+
+  for(;;){
+    sleep(30);
+  }
+
+  return 0;
 }
 
--- GPowerU/include/GPowerU.h
+++ GPowerU/include/GPowerU.h
@@ -1,5 +1,7 @@
 // 2023 INFN APE Lab - Sezione di Roma
 // cristian.rossi@roma1.infn.it
+// 2024 Istituto per le Applicazioni del Calcolo "Mauro Picone"
+// alessandro.celestini@cnr.it
 
 
 #ifndef _GPOWERU_H_
@@ -8,6 +10,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <string.h>
 #include <nvml.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -16,25 +19,22 @@
 #include <time.h>
 #include <sys/time.h>
 #include <sys/stat.h>
+#include <string>
 
 #include <cuda.h>
 
-#define SAMPLE_MAX_SIZE_DEFAULT 1000000
+using namespace std;
+
+//#define SAMPLE_MAX_SIZE_DEFAULT 1000000
+#define SAMPLE_MAX_SIZE_DEFAULT 2000000
 #define MAX_CHECKPOINTS 64
-#define MAX_DEVICES 4
+#define MAX_DEVICES 16
 
 #define ROOT_ENABLED 0
 #define MULTIGPU_DISABLED 0
 #define TIME_STEP 0.00001  //Interval for sampling (in s)
 #define POWER_THRESHOLD 0 
 
-#if ROOT_ENABLED
-#include "TCanvas.h"
-#include "TGraphErrors.h"
-#include "TF1.h"
-#include "TFile.h"
-#include "TLine.h"
-#endif
 
 
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -44,16 +44,7 @@
 void *threadWork(void*); //CPU thread managing the parallel power data taking during the kernel execution
 
 float DataOutput(); //Generate the output samples files
-int GPowerU_init(); //Initializations ==> enable the NVML library, starts CPU thread for the power monitoring
-
-#if ROOT_ENABLED
-void grapher(); //ROOT graph making function
-#endif
-
-
-__device__ void take_GPU_time(bool last); //Checkpoint power measure __device__ function 
-void GPowerU_checkpoints(); //Checkpoint power measure CPU function 
-
+int GPowerU_init(string out_dir, string node_name); //Initializations ==> enable the NVML library, starts CPU thread for the power monitoring
 int GPowerU_end(); //Ends power monitoring, returns data output files
 
 
@@ -61,10 +52,9 @@ int GPowerU_end(); //Ends power monitoring, returns data output files
  *              POWER_MEASURE GLOBAL VARIABLES       +
  *++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
-int terminate_thread; //END PROGRAM
+int terminate_thread = 0; //END PROGRAM
 
 nvmlDevice_t nvDevice[MAX_DEVICES];
-//__managed__ nvmlReturn_t nvResult;
 nvmlReturn_t nvResult;
 
 //Time sampling arrays for the power monitoring curve (thread_times) and kernel checkpoints (device_times)
@@ -90,12 +80,10 @@ unsigned int core_clock, mem_clock;
 
 float power_peak; //Maximum power value measured
 
-#if MULTIGPU_DISABLED
-//Variable used in the kernel checkpoint power data taking
-__managed__ int kernel_checkpoints[MAX_CHECKPOINTS]; 
-__managed__ int max_points;
-__managed__ bool finish;
-#endif
 unsigned int device_count;
+
+string glb_out_dir;
+string glb_node_name;
+string glb_file_starttime;
 
 #endif
